bb1f7a15d1d20bec286b6b0507f55116e3b90369
Elvis on Arch2 <elvios@gmail.com>
2014-11-25 13:55:54 +0100

    grouped it into genres

--- project.py
+++ project.py
@@ -3,24 +3,47 @@ import operator
 
 
 filename = "imdb-r.txt"
-save = False
-roles_dict = {}
+movie_genres = {}
+genres_roles = {}
 
 with open(filename) as f:
     for line in f:
-        if line.strip() == "LOCK TABLES `roles` WRITE;":
-            save = True
+        if line.strip()[0:10] == "LOCK TABLE":
+            type = line.strip()[13:-8].strip()
             continue
 
-        if save == True:
-            role = re.split(',', line)[2].strip()
-            #print(role)
-            if role in roles_dict:
-                roles_dict[role] += 1
+        
+
+        if type == "movies_genres":
+            movie_id = re.split(',', line)[0].strip()
+            genre = re.split(',', line)[1].strip().strip("'")
+            if movie_id in movie_genres:
+                movie_genres[movie_id].append(genre)
             else:
-                roles_dict[role] = 1
+                movie_genres[movie_id] = [genre]
 
-sorted_list = sorted(roles_dict.items(), key=operator.itemgetter(1))
+            if genre not in genres_roles:
+                genres_roles[genre] = {}
 
-for thing in sorted_list:
-    print(thing)
+
+        if type == "roles":
+            movie_id = re.split(',', line)[0].strip()
+            role = re.split(',', line)[2].strip().strip("'")
+            if movie_id in movie_genres:
+                genres = movie_genres[movie_id]
+
+                for genre in genres:
+                    if role in genres_roles[genre]:
+                        genres_roles[genre][role] += 1
+                    else:
+                        genres_roles[genre][role] = 1
+
+
+for g in genres_roles:
+    print("GENRE: " + g)
+
+    sorted_g = sorted(genres_roles[g].items(), key=operator.itemgetter(1))
+
+    for role in sorted_g:
+        print(role)
+
