ec35dc5542cfddc6a63f08a924fcf127a523cd27
Elvis on Arch2 <elvios@gmail.com>
2014-12-02 11:58:35 +0100

    Fiddling with new ways to calculate score

--- project.py
+++ project.py
@@ -15,12 +15,40 @@ def clean(role):
     role = re.split('\ \(', role)[0] # Takes care of 'Merchant (1990)'
     return role
 
+
+def calculate_rank(rankings, average):
+    n = float(len(rankings))
+    X = sum(rankings)/n
+    k = 10
+    C = average
+    return X*n/(n+k)+C*k/(n+k), len(rankings)
+    #return X, len(rankings)
+
+
+def calculate_score1(rankings, average, n_genre):
+    n = len(rankings)
+    score = n/n_genre*sum(rankings)/float(n)*100
+    return score, n
+
+
+def calculate_score2(rankings, average, n_genre):
+    rankings_good = []
+    rankings_bad = []
+    for ranking in rankings:
+        if ranking > average:
+            rankings_good.append(ranking)
+        else:
+            rankings_bad.append(ranking)
+
+    score = sum(rankings_good)/n_genre - sum(rankings_bad)/n_genre
+    return score*100, len(rankings_good), len(rankings_bad)
+
 filename = "imdb-r.txt"
 #filename = "toy.txt"
 movie_genres = {}
 genres_roles = {}
 movie_rankings = {}
-average = 0
+all_rankings = {}
 
 with open(filename) as f:
     for line in f:
@@ -35,6 +63,8 @@ with open(filename) as f:
 
             if rank == "NULL":
                 continue
+
+            rank = float(rank)
             
             movie_rankings[movie_id] = rank
                 
@@ -51,6 +81,7 @@ with open(filename) as f:
 
             if genre not in genres_roles:
                 genres_roles[genre] = {}
+                all_rankings[genre] = []
 
 
         if tabletype == "roles":
@@ -71,19 +102,27 @@ with open(filename) as f:
                 genres = movie_genres[movie_id]
 
                 for genre in genres:
+                    all_rankings[genre].append(rank)
                     if role in genres_roles[genre]:
-                        count = genres_roles[genre][role][1]
-                        avg = genres_roles[genre][role][0]
-                        genres_roles[genre][role][1] += 1
-                        genres_roles[genre][role][0] = (avg*count+rank)/(count+1)
+                        genres_roles[genre][role].append(rank)
                     else:
-                        genres_roles[genre][role] = [rank, 1]
+                        genres_roles[genre][role] = [rank]
             #else:
                 #print(str(movie_id) + " is not in movie_genres dictionary")
 
 
+
 for g in sorted(genres_roles):
-    print("GENRE: " + g)
+
+    average = sum(all_rankings[g])/float(len(all_rankings[g]))
+    n_genre = len(all_rankings[g])
+
+    for role in genres_roles[g]:
+        #genres_roles[g][role] = calculate_rank(genres_roles[g][role], average)
+        #genres_roles[g][role] = calculate_score1(genres_roles[g][role], average, n_genre)  
+        genres_roles[g][role] = calculate_score2(genres_roles[g][role], average, n_genre)  
+
+    print("GENRE: " + g + "   avg: " + str(average))
 
     sorted_g = sorted(genres_roles[g].items(), key=operator.itemgetter(1), reverse=True)
 
